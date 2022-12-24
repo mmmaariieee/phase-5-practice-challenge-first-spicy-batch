aea731376c835e5e717a1d70404ac821239145c7
Mariia <m.goncharenkoo@icloud.com>
2022-12-23 20:44:24 -0500

    passed tests

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -4,5 +4,33 @@
 # def is_spicy_batch(batch):
 
 def first_spicy_batch(n)
-  # your code here
+
+  # Time: O(n)
+  # Space: O(1)
+
+  # n = 0
+  # while !is_spicy_batch(n)
+  #   n += 1
+  # end
+  # n
+
+  # Time: O(log n)
+  # Space: O(1)
+
+  left = 0
+  right = n
+
+  while left < right
+
+    mid = left + (right - left) / 2
+
+    if is_spicy_batch(mid)
+      right = mid
+    else
+      left = mid + 1
+    end
+  end
+
+  left
+
 end
